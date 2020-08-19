ccee1c5cd25def97403ca2df5d1e9953a2fefe3c
IkehAkinyemi <mrikehchukwka@gmail.com>
2020-08-20 00:07:02 +0100

    Created an actual infinite content

--- main.js
+++ main.js
@@ -3,30 +3,28 @@ let firstUI = document.querySelector('.first');
 let secondUI = document.querySelector('.second');
 let thirdUI = document.querySelector('.third');
 let text = document.querySelector('.text');
- 
+
 let num = 0;
 let time = 1000;
 let post = ['Third.png', 'Second.png', 'First.png'];
 
  function myFunction() {
-    for (let x=0; x < 100; x++) {
+	setInterval(function() {
 		let randomPost = Math.floor(Math.random() * ( post.length));
-		console.log(randomPost);
-		const generatedDIV = document.createElement('div');
-		generatedDIV.className = 'first';
-		const imgContainer = document.createElement('div');
-		imgContainer.className = 'img-container';
-		imgContainer.innerHTML = `<img src="${post[randomPost]}">`;
-		let textContainer = document.createElement('div');
-		textContainer.className = 'text';
-		let para = document.createElement('p');
-		para.textContent = `${text.firstElementChild.textContent}`;
-		textContainer.appendChild(para);
-		generatedDIV.appendChild(imgContainer);
-		generatedDIV.appendChild(textContainer);
-		mContainer.appendChild(generatedDIV);
-		
-    }
+			const generatedDIV = document.createElement('div');
+			generatedDIV.className = 'first';
+			const imgContainer = document.createElement('div');
+			imgContainer.className = 'img-container';
+			imgContainer.innerHTML = `<img src="${post[randomPost]}">`;
+			let textContainer = document.createElement('div');
+			textContainer.className = 'text';
+			let para = document.createElement('p');
+			para.textContent = `${text.firstElementChild.textContent}`;
+			textContainer.appendChild(para);
+			generatedDIV.appendChild(imgContainer);
+			generatedDIV.appendChild(textContainer);
+			mContainer.appendChild(generatedDIV);
+	}, 3000)
  }
 
 
@@ -34,10 +32,3 @@ function loadEvents() {
 	document.addEventListener('DOMContentLoaded', myFunction);
 }
 loadEvents();
-
-
-
-
-
-
-
