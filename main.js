3c8e6101c264a78ab86dae4ee582772ea161eda4
IkehAkinyemi <mrikehchukwka@gmail.com>
2020-08-23 00:44:09 +0100

    Built a responsive UI and also added a loading effect for UX

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
+//UI variables
 let mContainer = document.querySelector('.container');
 let firstUI = document.querySelector('.first');
 let secondUI = document.querySelector('.second');
 let thirdUI = document.querySelector('.third');
 let text = document.querySelector('.text');
+let UIloader = document.querySelector('.loading');
 
+// Local variables
 let num = 0;
 let time = 1000;
 let post = ['Third.png', 'Second.png', 'First.png'];
+let postLimit = 5;
 
- function myFunction() {
+//Function causing an infinite posting, resulting in infinite scrolling
+function myFunction() {
 	setInterval(function() {
-		let randomPost = Math.floor(Math.random() * ( post.length));
+		setTimeout(function(){
+			UIloader.style.display = 'none';
+		}, 4000)
+		let postsPerSeconds = Math.floor(Math.random() * postLimit);
+		for(let x = 0; x < postsPerSeconds; x++) {
+			let randomPost = Math.floor(Math.random() * (post.length));
 			const generatedDIV = document.createElement('div');
 			generatedDIV.className = 'first';
 			const imgContainer = document.createElement('div');
@@ -24,10 +34,14 @@ let post = ['Third.png', 'Second.png', 'First.png'];
 			generatedDIV.appendChild(imgContainer);
 			generatedDIV.appendChild(textContainer);
 			mContainer.appendChild(generatedDIV);
-	}, 3000)
- }
+		};
+		setTimeout(function() {
+			UIloader.style.display = 'block';
+		}, 100)
+	}, 2000)
+};
 
-
+//Load all available events
 function loadEvents() {
 	document.addEventListener('DOMContentLoaded', myFunction);
 }
